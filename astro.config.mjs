9fe66715af8ceb4d4bebaef366bd7fa5aa42dee6
Trevor Lee <49603972+trevortylerlee@users.noreply.github.com>
2024-11-03 14:06:19 -0800

    add pagefind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import pagefind from "astro-pagefind";
 
-// https://astro.build/config
 export default defineConfig({
-  // change this to your own domain
   site: "https://astro-byte.vercel.app",
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind(), sitemap(), pagefind()],
 });
